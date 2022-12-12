f658be0d8da5734a73768f2da38585f6eada7ccc
Its-Cybert <jseybert.dev@gmail.com>
2022-12-12 10:07:52 -0500

    Modified Store.java, mising a bracket. Created and am adding Order.java, shows if-then-else and switch statments in java.

---
+++ Order/Order.java
@@ -0,0 +1,55 @@
+public class Order {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+    
+    public Order(boolean filled, double cost, String shippingMethod) {
+          if (cost > 24.00) {
+        System.out.println("High value item!");
+      } else {
+        System.out.println("Low value item!");
+      }
+      isFilled = filled;
+      billAmount = cost;
+      shipping = shippingMethod;
+    }
+    
+    public void ship() {
+      if (isFilled) {
+        System.out.println("Shipping");
+      } else {
+        System.out.println("Order not ready");
+      }
+      
+      double shippingCost = calculateShipping();
+      
+      System.out.println("Shipping cost: ");
+      System.out.println(shippingCost);
+    }
+    
+    public double calculateShipping() {
+      double shippingCost;
+      switch (shipping) {
+        case "Regular":
+          shippingCost = 0;
+          break;
+        case "Express":    
+          shippingCost = 1.75;
+          break;
+        default:
+          shippingCost = .50; 
+      }
+      return shippingCost;
+       }
+    
+    public static void main(String[] args) {
+      // create instances and call methods here!
+      Order book = new Order(true, 9.99, "Express");
+      Order chemistrySet = new Order(false, 72.50, "Regular");
+      Order christmas = new Order (true, 200.99, "Other");
+      
+      book.ship();
+      chemistrySet.ship();
+      christmas.ship();
+    }
+  }
--- Store/Store.java
+++ Store/Store.java
@@ -29,3 +29,4 @@ public class Store {
       System.out.println(lemonadePrice);
   
     }
+  }
