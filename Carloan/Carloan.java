d6115c3665efe63008d658173080d7f6541b123a
Its-Cybert <jseybert.dev@gmail.com>
2022-12-12 15:40:49 -0500

    Fixed error with repo being an add on inside the repo, also re added car loan as it was losed to nested root folder, and additoanlly added Continents Java program.

---
+++ Carloan/Carloan.java
@@ -0,0 +1,26 @@
+/*
+A basic Car loan calculate, input the desired informtion in the beginning to calcualte your monthly payments on your new whip. (does do light error checking)
+*/
+public class CarLoan {
+	public static void main(String[] args) {
+    int carLoan = 64000;
+    int loanLength = 6;
+    int interestRate = 3;
+    int downPayment = 0;
+
+    if (loanLength <= 0 || interestRate <=0){
+      System.out.println("Error! You must make a valid car loan.");
+    }else if (downPayment > carLoan){
+      System.out.println("The car can be paid in full!");
+    }else{
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLength * 12;
+      int monthlyBalance = remainingBalance / months;
+      int interest = monthlyBalance * interestRate / 100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println("Your monthly payment would be "+ monthlyPayment+ "!");
+    }
+	
+
+	}
+}
